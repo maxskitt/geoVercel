b249e47b1c4fd26344d007dd00a2b80973ecee96
Maksim Ponomarev <max.p@g-code.cc>
2024-02-22 16:30:18 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { withCountryInfo } from "./scripts/countries.js";
+
+const nextConfig = withCountryInfo({});
 
 export default nextConfig;
